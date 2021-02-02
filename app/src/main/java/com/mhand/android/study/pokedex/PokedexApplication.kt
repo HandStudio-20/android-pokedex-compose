31bf35e6b3e9a2bdc0155710303e4919ad56e3f5
duyeong <20@mhand.net>
2021-02-02 10:48:53 +0900

    Hilt Application 적용

---
+++ app/src/main/java/com/mhand/android/study/pokedex/PokedexApplication.kt
@@ -0,0 +1,11 @@
+package com.mhand.android.study.pokedex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokedexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
