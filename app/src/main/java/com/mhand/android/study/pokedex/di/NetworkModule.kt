1773285b0432d8d94858fc2a8e3b09b4adfd71ab
duyeong <20@mhand.net>
2021-02-02 11:20:13 +0900

    Retrofit, OkHttp Hilt 모듈 작성

---
+++ app/src/main/java/com/mhand/android/study/pokedex/di/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.mhand.android.study.pokedex.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideBaseUrl() = "https://pokeapi.co/api/v2/"
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient() =
+        OkHttpClient.Builder()
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, baseUrl: String): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .build()
+
+
+}
